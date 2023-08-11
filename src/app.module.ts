34aca23c3cb2ea5b89d3a3a19900611ebc23f94a
Hanan11240 <peerzadahanannaseer@gmail.com>
2023-08-11 08:52:47 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+   envFilePath: `.env.${process.env.NODE_ENV}`,
+    isGlobal: true,
+    cache: true,
+  }),
+  MongooseModule.forRootAsync({
+    inject:[ConfigService],
+    useFactory: (configService: ConfigService) => {
+      const resolvedMongoUri = configService.get<string>('MONGODB_URI');
+      console.log('Resolved MongoDB URI:', resolvedMongoUri);
+      return {
+        uri: resolvedMongoUri,
+      }; 
+    },
+  })
+],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  
+}
