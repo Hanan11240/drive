36d1d7613108b7b993bdb6f3209cdf937f2e2201
Hanan11240 <peerzadahanannaseer@gmail.com>
2023-08-19 16:09:18 +0530

    add client-side

--- server/src/folders/schema/folder.schema.ts
+++ server/src/folders/schema/folder.schema.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export const  FolderSchema= new mongoose.Schema({
         folderName:String,
         fileIds:[mongoose.Schema.Types.ObjectId],
-        parentFolderId:{trpe:mongoose.Schema.Types.ObjectId,default:null},
+        parentFolderId:{type:mongoose.Schema.Types.ObjectId,default:null},
         folderIds:[mongoose.Schema.Types.ObjectId],
         childNumber:{type:Number,default:0}
 })
