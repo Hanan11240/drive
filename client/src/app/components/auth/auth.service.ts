da39b27382898058b30c2d1424db51aef58df486
Peerzada Hanan Naseer <66436039+Hanan11240@users.noreply.github.com>
2023-08-30 05:23:00 +0000

    removed const

--- client/src/app/components/auth/auth.service.ts
+++ client/src/app/components/auth/auth.service.ts
@@ -9,7 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class AuthService {
 
-  const serverUrl = environment.serverUrl
+  serverUrl = environment.serverUrl
   constructor(private http:HttpClient) { }
 
 
