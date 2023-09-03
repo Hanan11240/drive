73c639910bb295f26b71f63c107023fe07d56670
Hanan11240 <peerzadahanannaseer@gmail.com>
2023-09-03 15:44:23 +0530

    add mat menu

--- client/src/app/components/view-folders/view-folders.component.ts
+++ client/src/app/components/view-folders/view-folders.component.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { FolderModel } from './models/folder';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-view-folders',
   standalone: true,
-  imports: [CommonModule,MatIconModule],
+  imports: [CommonModule,MatIconModule,MatMenuModule,MatButtonModule],
   templateUrl: './view-folders.component.html',
   styleUrls: ['./view-folders.component.scss']
 })
